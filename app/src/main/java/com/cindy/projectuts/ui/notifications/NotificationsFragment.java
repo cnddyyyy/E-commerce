8f21026010d880335eea63ec7423d010b43a051e
cnddyyyy <dyystudies@gmail.com>
2024-11-25 16:51:34 +0800

    Project UTS

--- app/src/main/java/com/cindy/projectuts/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/cindy/projectuts/ui/notifications/NotificationsFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.cindy.projectuts.R;
 import com.cindy.projectuts.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
--- app/src/main/java/com/cindy/projectuts/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/cindy/projectuts/ui/profile/ProfileFragment.java
@@ -4,36 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.cindy.projectuts.databinding.FragmentProfileBinding;
+import com.cindy.projectuts.R;
 
 public class ProfileFragment extends Fragment {
-    private FragmentProfileBinding binding;
-
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ProfileViewModel profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        // Bind views
+        ImageView profileImage = root.findViewById(R.id.profile_image);
+        TextView profileName = root.findViewById(R.id.profile_name);
+        Button detailButton = root.findViewById(R.id.detail_button);
 
-        final TextView textView = binding.textProfile;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Set default data
+        profileName.setText("Anne Smith");
+        profileImage.setImageResource(R.drawable.ic_place_holder);
+
+        // Add button click listener
+        detailButton.setOnClickListener(v -> {
+            // Perform action on button click
+        });
         return root;
     }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
+
